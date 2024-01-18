73d3ff9c6be93af645ec8d3fa7c4f79c536b6b9b
yuki-noguchi <y.n.10191@gmail.com>
2024-01-18 18:16:50 +0900

    TODOアプリを作成

--- lib/main.dart
+++ lib/main.dart
@@ -31,95 +31,104 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TodoList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _TodoListPage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _TodoListPage extends State<TodoList> {
+  List<String> todos = [];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: const NavigationBar(title: "TODOリスト"),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todos[index]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () async {
+          var newTodo = await Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const TodoAddPage()));
+          if (newTodo != null) {
+            setState(() {
+              todos.add(newTodo);
+            });
+          }
+        },
+        shape: const CircleBorder(),
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+}
+
+class NavigationBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const NavigationBar({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        title: Text(title, style: const TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue);
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class TodoAddPage extends StatefulWidget {
+  const TodoAddPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _TodoAddPage();
+}
+
+class _TodoAddPage extends State<TodoAddPage> {
+  String _todoTitle = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: const NavigationBar(title: "TODOを追加"),
+        body: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 64.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              TextField(
+                autofocus: true,
+                decoration: const InputDecoration(hintText: "タイトルを入力"),
+                onChanged: (value) {
+                  setState(() {
+                    _todoTitle = value;
+                  });
+                },
+              ), //
+              const SizedBox(
+                height: 32,
+              ),
+              ElevatedButton(
+                  onPressed: () => Navigator.pop(context, _todoTitle),
+                  child: const Text("TODOを追加する")),
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text("キャンセル"))
+            ])));
   }
 }
